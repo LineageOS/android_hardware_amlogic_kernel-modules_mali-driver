7b7e5fa98e25b657ab03030cf083054764b795c5
Jiyu Yang <jiyu.yang@amlogic.com>
2016-05-19 22:51:38 +0800

    PD#125571 gpu: update mk
    Change-Id: I96c5f6b4162c5eebef0f4b91535a6781e8eb3baa

--- gpu.mk
+++ gpu.mk
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+include device/amlogic/$(TARGET_PRODUCT)/BoardConfig.mk
+
 LOCAL_KK=0
 ifeq ($(GPU_TYPE), t82x)
 	LOCAL_KK=1
@@ -22,46 +24,50 @@ ifeq ($(GPU_TYPE), t83x)
 	LOCAL_KK=1
 endif
 
+$(info test  GPU_TYPE is $(GPU_TYPE))
 ifeq ($(LOCAL_KK),0)
 $(info test  LOCAL_KK is $(LOCAL_KK))
 MALI=hardware/arm/gpu/mali
-MALI_OUT=hardware/arm/gpu/mali
+MALI_OUT=$(PRODUCT_OUT)/obj/
 KERNEL_ARCH ?= arm
-
+#TODO rm shell cmd
 define gpu-modules
 
 $(MALI_KO):
+	cp $(MALI) $(MALI_OUT) -rf
 	@echo "make mali module KERNEL_ARCH is $(KERNEL_ARCH)"
-	$(MAKE) -C $(shell pwd)/$(PRODUCT_OUT)/obj/KERNEL_OBJ M=$(shell pwd)/$(MALI)		\
+	@echo "make mali module MALI_OUT is $(MALI_OUT)"
+	@echo "make mali module MAKE is $(MAKE)"
+	$(MAKE) -C $(shell pwd)/$(PRODUCT_OUT)/obj/KERNEL_OBJ M=$(shell pwd)/$(MALI_OUT)/mali \
 	ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(PREFIX_CROSS_COMPILE) CONFIG_MALI400=m  CONFIG_MALI450=m 	\
+	EXTRA_CFLAGS="-DCONFIG_MALI400=m -DCONFIG_MALI450=m" \
 	CONFIG_GPU_THERMAL=y CONFIG_AM_VDEC_H264_4K2K=y modules
 
 	mkdir -p $(PRODUCT_OUT)/root/boot
-	cp $(MALI_OUT)/mali.ko $(PRODUCT_OUT)/root/boot/mali.ko
+	cp $(MALI_OUT)/mali/mali.ko $(PRODUCT_OUT)/root/boot/mali.ko
 endef
 
 else
 
-$(info test  LOCAL_KK is $LOCAL_KK)
-MALI=hardware/arm/gpu/t83x/kernel/drivers/gpu/arm/midgard
-MALI_OUT=hardware/arm/gpu/t83x/kernel/drivers/gpu/arm/midgard
+$(info test  LOCAL_KK is $(LOCAL_KK))
+MALI=hardware/arm/gpu/t83x
+MALI_OUT=$(PRODUCT_OUT)/obj/t83x
 KERNEL_ARCH ?= arm
 
 define gpu-modules
 
 $(MALI_KO):
+	cp $(MALI) $(MALI_OUT) -rf
 	@echo "make mali module KERNEL_ARCH is $(KERNEL_ARCH) current dir is $(shell pwd)"
 	@echo "MALI is $(MALI), MALI_OUT is $(MALI_OUT)"
-	$(MAKE) -C $(shell pwd)/$(PRODUCT_OUT)/obj/KERNEL_OBJ M=$(shell pwd)/$(MALI)	  \
+	$(MAKE) -C $(shell pwd)/$(PRODUCT_OUT)/obj/KERNEL_OBJ M=$(shell pwd)/$(MALI_OUT)/kernel/drivers/gpu/arm/midgard \
 	ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(PREFIX_CROSS_COMPILE) \
 	EXTRA_CFLAGS="-DCONFIG_MALI_PLATFORM_DEVICETREE -DCONFIG_MALI_MIDGARD_DVFS -DCONFIG_MALI_BACKEND=gpu" \
 	CONFIG_MALI_MIDGARD=m CONFIG_MALI_PLATFORM_DEVICETREE=y CONFIG_MALI_MIDGARD_DVFS=y CONFIG_MALI_BACKEND=gpu modules
 
 	mkdir -p $(PRODUCT_OUT)/root/boot
-	cp $(MALI_OUT)/mali_kbase.ko $(PRODUCT_OUT)/root/boot/mali.ko
+	cp $(MALI_OUT)/kernel/drivers/gpu/arm/midgard/mali_kbase.ko $(PRODUCT_OUT)/root/boot/mali.ko
 	$(cd -)
 	@echo "make mali module finished current dir is $(shell pwd)"
 endef
 endif
-$(info local path is $(LOCAL_PATH))
-#include hardware/arm/gpu/lib/lib.mk
