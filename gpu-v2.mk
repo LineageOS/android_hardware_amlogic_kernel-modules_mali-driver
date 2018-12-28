8b27d100f08b92ee3c18533c006f70a1040bf283
Shuide Chen <shuide.chen@amlogic.com>
2018-12-28 14:39:02 +0800

    Q-bringup: fix gpu build error for Q [6/8]
    PD#SWPL-3168
    
    Problem:
    cannot build Q source.
    
    Solution:
    fix build requirement for Q， as:
    https://android.googlesource.com/platform/build/+/master/Changes.md
    rearrange the module target
    
    Verify:
    locally and patchbuild.
    
    Change-Id: Ie93f0a4baf0c2838d320ba6ad58214bab45e161c
    Signed-off-by: Shuide Chen <shuide.chen@amlogic.com>

--- gpu-v2.mk
+++ gpu-v2.mk
@@ -19,7 +19,7 @@ GPU_MODS_OUT?=system/lib
 KERNEL_ARCH ?= arm
 GPU_DRV_VERSION?=r6p1
 
-$(PRODUCT_OUT)/obj/lib_vendor/mali.ko: $(GPU_ARCH).ko
+$(PRODUCT_OUT)/obj/lib_vendor/mali.ko: $(PRODUCT_OUT)/$(GPU_MODS_OUT)/$(GPU_ARCH).ko
 	-cp  $(PRODUCT_OUT)/$(GPU_MODS_OUT)/mali.ko $(PRODUCT_OUT)/obj/lib_vendor/mali.ko
 	echo "$(GPU_ARCH).ko build finished"
 
@@ -34,6 +34,7 @@ define utgard-modules
 	@echo "make mali module MALI_OUT is $(PRODUCT_OUT)/obj/mali $(MALI_OUT)"
 	@echo "make mali module MAKE is $(MAKE)"
 	@echo "GPU_DRV_VERSION is $(1)"
+	PATH=$$(cd ./$(TARGET_HOST_TOOL_PATH); pwd):$$PATH \
 	$(MAKE) -C $(shell pwd)/$(PRODUCT_OUT)/obj/KERNEL_OBJ M=$(shell pwd)/$(PRODUCT_OUT)/obj/mali  \
 	ARCH=$(3) CROSS_COMPILE=$(PREFIX_CROSS_COMPILE) CONFIG_MALI400=m  CONFIG_MALI450=m    \
 	EXTRA_CFLAGS="-DCONFIG_MALI400=m -DCONFIG_MALI450=m" \
@@ -52,6 +53,7 @@ define midgard-modules
 	cp $(2)/* $(PRODUCT_OUT)/obj/t83x -airf
 	@echo "make mali module KERNEL_ARCH is $(KERNEL_ARCH) current dir is $(shell pwd)"
 	@echo "MALI is $(2), MALI_OUT is $(PRODUCT_OUT)/obj/t83x "
+	PATH=$$(cd ./$(TARGET_HOST_TOOL_PATH); pwd):$$PATH \
 	$(MAKE) -C $(shell pwd)/$(PRODUCT_OUT)/obj/KERNEL_OBJ M=$(shell pwd)/$(PRODUCT_OUT)/obj/t83x/kernel/drivers/gpu/arm/midgard \
 	ARCH=$(3) CROSS_COMPILE=$(PREFIX_CROSS_COMPILE) \
 	EXTRA_CFLAGS="-DCONFIG_MALI_PLATFORM_DEVICETREE -DCONFIG_MALI_MIDGARD_DVFS -DCONFIG_MALI_BACKEND=gpu" \
@@ -71,6 +73,7 @@ define bifrost-modules
 	cp $(2)/* $(PRODUCT_OUT)/obj/bifrost -airf
 	@echo "make mali module KERNEL_ARCH is $(KERNEL_ARCH) current dir is $(shell pwd)"
 	@echo "MALI is $(2), MALI_OUT is $(PRODUCT_OUT)/obj/bifrost "
+	PATH=$$(cd ./$(TARGET_HOST_TOOL_PATH); pwd):$$PATH \
 	$(MAKE) -C $(shell pwd)/$(PRODUCT_OUT)/obj/KERNEL_OBJ M=$(shell pwd)/$(PRODUCT_OUT)/obj/bifrost/kernel/drivers/gpu/arm/midgard \
 	ARCH=$(3) CROSS_COMPILE=$(PREFIX_CROSS_COMPILE) \
 	EXTRA_CFLAGS="-DCONFIG_MALI_PLATFORM_DEVICETREE -DCONFIG_MALI_MIDGARD_DVFS -DCONFIG_MALI_BACKEND=gpu " \
@@ -84,11 +87,13 @@ define bifrost-modules
 	@echo "make mali module finished current dir is $(shell pwd)"
 endef
 
-bifrost.ko: $(INSTALLED_KERNEL_TARGET)
+# start from Q, only support build the module in 'out' directory
+# modify to fit this requirement
+$(PRODUCT_OUT)/$(GPU_MODS_OUT)/bifrost.ko: $(INSTALLED_KERNEL_TARGET)
 	$(call bifrost-modules,$(MESON_GPU_DIR),$(MESON_GPU_DIR)/bifrost/$(GPU_DRV_VERSION),$(KERNEL_ARCH))
 
-midgard.ko: $(INSTALLED_KERNEL_TARGET)
+$(PRODUCT_OUT)/$(GPU_MODS_OUT)/midgard.ko: $(INSTALLED_KERNEL_TARGET)
 	$(call midgard-modules,$(MESON_GPU_DIR),$(MESON_GPU_DIR)/midgard/$(GPU_DRV_VERSION),$(KERNEL_ARCH))
 
-utgard.ko: $(INSTALLED_KERNEL_TARGET)
+$(PRODUCT_OUT)/$(GPU_MODS_OUT)/utgard.ko: $(INSTALLED_KERNEL_TARGET)
 	$(call utgard-modules,$(MESON_GPU_DIR),$(MESON_GPU_DIR)/utgard/$(GPU_DRV_VERSION),$(KERNEL_ARCH))
