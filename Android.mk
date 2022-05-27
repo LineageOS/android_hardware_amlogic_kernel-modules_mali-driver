e436241062014f098c12318033385bdc4e5a2fe8
Vitaliy Tomin <highwaystar.ru@gmail.com>
2022-05-28 00:57:40 +0800

    Split mali gpu makefiles per GPU arch
    * use TARGET_AMLOGIC_GPU_ARCH flag set to bifrost, midgard or utgard to
      select proper mali module to build
    
    Change-Id: I1b32483fc32e2709688013a2941baa877484dee5

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,40 +14,7 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-MALI_GPU_VARIANT ?= bifrost
-MALI_DRV_VERSION ?= r32p1
-MALI_PATH        := $(abspath $(call my-dir))
-GPU_PATH         := $(MALI_GPU_VARIANT)/$(MALI_DRV_VERSION)/kernel/drivers/gpu/arm/midgard
-GPU_CONFIGS      := CONFIG_MALI_MIDGARD=m CONFIG_MALI_MIDGARD_DVFS=y CONFIG_MALI_DMA_BUF_MAP_ON_DEMAND=y CONFIG_MALI_BACKEND=gpu
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE        := mali
-LOCAL_MODULE_SUFFIX := .ko
-LOCAL_MODULE_CLASS  := ETC
-LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/lib/modules
-LOCAL_CFLAGS        := -Wno-error -Wno-pointer-sign -Wno-error=frame-larger-than=
-LOCAL_C_INCLUDES    := \
-    $(MALI_PATH)/$(GPU_PATH)/backend/gpu \
-    $(MALI_PATH)/$(GPU_PATH)/platform/devicetree \
-    $(MALI_PATH)/$(GPU_PATH)/ipa \
-    $(MALI_PATH)/$(GPU_PATH) \
-    $(MALI_PATH)/dvalin/kernel/include
-
-_mali_intermediates := $(call intermediates-dir-for,$(LOCAL_MODULE_CLASS),$(LOCAL_MODULE))
-_mali_ko := $(_mali_intermediates)/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
-KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
-GPU_CFLAGS := $(LOCAL_CFLAGS) $(addprefix -I,$(LOCAL_C_INCLUDES)) -DCONFIG_MALI_PLATFORM_DEVICETREE -DCONFIG_MALI_MIDGARD_DVFS -DCONFIG_MALI_BACKEND=gpu -DCONFIG_MALI_DMA_BUF_MAP_ON_DEMAND=1 -DCONFIG_MALI_DMA_BUF_LEGACY_COMPAT=0
-
-$(_mali_ko): $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/$(BOARD_KERNEL_IMAGE_NAME)
-	@mkdir -p $(dir $@)
-	@cp -R $(MALI_PATH)/* $(dir $@)/
-	$(hide) +$(KERNEL_MAKE_CMD) $(PATH_OVERRIDE) $(KERNEL_MAKE_FLAGS) -C $(KERNEL_OUT) M=$(abspath $(_mali_intermediates))/$(GPU_PATH) ARCH=$(TARGET_KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) EXTRA_CFLAGS="$(GPU_CFLAGS)" $(GPU_CONFIGS) modules
-	$(KERNEL_TOOLCHAIN_PATH)strip --strip-unneeded $(dir $@)/$(GPU_PATH)/mali_kbase.ko; \
-	cp $(dir $@)/$(GPU_PATH)/mali_kbase.ko $@;
-
-include $(BUILD_SYSTEM)/base_rules.mk
+ifneq ($(filter bifrost midgard utgard,$(TARGET_AMLOGIC_GPU_ARCH)),)
+MALI_PATH := $(abspath $(call my-dir))
+include  $(abspath $(call my-dir))/$(TARGET_AMLOGIC_GPU_ARCH)/Android.mk
 endif
